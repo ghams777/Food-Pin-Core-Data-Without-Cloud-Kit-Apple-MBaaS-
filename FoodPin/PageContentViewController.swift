5be001593092ebaadccf4408d89eff5959bfa282
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-04 14:45:41 +0700

    Membuat Class PageContentViewController sukses.
    Signed-off-by: Managam Silalahi <managam.silalahi@gmail.com>

---
+++ FoodPin/PageContentViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PageContentViewController.swift
+//  FoodPin
+//
+//  Created by Admin on 4/4/16.
+//  Copyright © 2016 Morra. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+    
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var subHeadingLabel: UILabel!
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    var index: Int = 0
+    var heading: String = ""
+    var subHeading: String = ""
+    var imageFile: String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
