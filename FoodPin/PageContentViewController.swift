88598cfe227d2c79a1886d73812c8f95ad312468
Managam Silalahi <managam.silalahi@gmail.com>
2016-04-04 17:33:09 +0700

    Menambahkan Tombol Next dan Get Started sukses.
    Signed-off-by: Managam Silalahi <managam.silalahi@gmail.com>

--- FoodPin/PageContentViewController.swift
+++ FoodPin/PageContentViewController.swift
@@ -14,6 +14,8 @@ class PageContentViewController: UIViewController {
     @IBOutlet weak var subHeadingLabel: UILabel!
     @IBOutlet weak var contentImageView: UIImageView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var getStartedButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
     
     var index: Int = 0
     var heading: String = ""
@@ -32,6 +34,9 @@ class PageContentViewController: UIViewController {
         // set halaman page control
         pageControl.currentPage = index
         
+        //show hidden get started and forward button
+        getStartedButton.hidden = (index == 2) ? false : true
+        forwardButton.hidden = (index == 2) ? true : false
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,6 +44,10 @@ class PageContentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func nextScreen(sender: AnyObject) {
+        let pageViewController = self.parentViewController as! PageViewController
+        pageViewController.forward(index)
+    }
 
     /*
     // MARK: - Navigation
